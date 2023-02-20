8705a424157468d89d44f7f988cdf3e71cdcfd21
e.demin <demsq@mail.ru>
2023-02-20 13:27:14 +0300

    Complete 6.1

--- HW_6.1/Program.cs
+++ HW_6.1/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿// ДЗ 6.1 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Инициализация программы");
+int leng = ReadInt("количество чисел");
+int [] array = new int[leng];
+array = getArray(array);
+Console.WriteLine(string.Join(", ", array));
+checkResult(array);
+
+int ReadInt(string argumrntName)
+{
+    Console.Write($"Введите {argumrntName}: ");
+    
+    int number = 0;
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("Вы ввели не число! Введите число!");
+    }
+
+    return number;
+}
+
+int[] getArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = ReadInt($"number {i+1} ");
+    }
+
+    return array;
+}
+
+void checkResult(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count +=1;
+        }
+    }
+    Console.WriteLine($"Количество чисел больше 0 = {count}");
+}
+Console.WriteLine("Программа завершила работу");
